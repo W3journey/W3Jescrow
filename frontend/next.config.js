08edf03d2745f4d8344b7d3c727606971cec595d
W3journey <web3journey@proton.me>
2023-10-05 11:00:19 +0200

    cleaned up after ethersjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,11 +6,6 @@ const nextConfig = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     config.externals.push("pino-pretty", "lokijs", "encoding")
-    // TODO remove next line when ethersjs is replaced
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
-    })
     return config
   },
 }
